955a2103bb9755191fb77ba56bf0b4c5157bdbfd
Talita <talitasantos@visaojr.com.br>
2017-08-22 10:20:25 -0300

    ACABOOOOOOOU

--- src/boyermoore/Algoritmo.java
+++ src/boyermoore/Algoritmo.java
@@ -2,26 +2,20 @@ package boyermoore;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-//import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
-//import java.io.FileReader;
-//import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import static java.lang.Math.abs;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Algoritmo {
 
     protected char[] alfabeto = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z'};
-    protected int[] valor = new int[alfabeto.length];//valor e um vetor que representa o peso de cada letra do alfabeto
-    protected int tam;//tamanho da palavra chave
+    protected int[] valor = new int[alfabeto.length];//valor de um vetor que representa o peso de cada letra do alfabeto
+    protected int tam;//tamanho da primeira palavra chave
     protected char[] chave;//vetor da palavra chave
+    protected char[] chave2;//vetor da segunda palavra chave
     protected String linha;//String referente a cada linha do arquivo lido
     protected char[] frase;//vetor da linha
 
@@ -39,11 +33,11 @@ public class Algoritmo {
         }
 
 //        System.out.println("Vetor de valores do alfabeto: ");
-//        System.out.print(" ( ");
+//        System.out.print(" [ ");
 //        for (int i = 0; i < alfabeto.length; i++) {
 //            System.out.print(valor[i] + ",");
 //        }
-//        System.out.print(" ) ");
+//        System.out.print(" ] ");
         //insercao da palavra chave em um vetor
         chave = key.toCharArray();
 
@@ -60,30 +54,25 @@ public class Algoritmo {
         }
 
 //        System.out.println("\nVetor de valores do alfabeto após a atualização: ");
-//        System.out.print(" ( ");
+//        System.out.print(" [ ");
 //        for (int i = 0; i < alfabeto.length; i++) {
 //            System.out.print(valor[i] + ",");
 //        }
-//        System.out.print(" ) ");
+//        System.out.print(" ] ");
     }
 
-    /* funcao que faz a leitura do arquivo entrada01.txt e faz a chamada da funcao buscaPalavra(String linha) para
+    /* funcao que faz a leitura do arquivo entrada1.txt e faz a chamada da funcao buscaPalavra(String linha, int num, String key2) para
     cada linha do arquivo */
-    public void leArquivo() {
+    public void leArquivo(String key2) {
         try {
-            /* nao aceita caracteres especiais
             FileReader arq = new FileReader("entrada1.txt");
             BufferedReader ent = new BufferedReader(arq);
-             */
 
-            //aceita caracteres especiais
-            BufferedReader ent = new BufferedReader(new InputStreamReader(new FileInputStream("entrada1.txt"), "ISO-8859-1"));
-
-            int num = 0;//linha do arquivo
+            int num = 1;//linha do arquivo
             while (ent.ready()) {
-                num++;
                 linha = ent.readLine();
-                buscaPalavra(linha, num);
+                buscaPalavra(linha, num, key2);
+                num++;
             }
             ent.close();
         } catch (IOException e) {
@@ -93,8 +82,9 @@ public class Algoritmo {
     }
 
     /* funcao que busca por ocorrencias da palavra chave em cada linha do arquivo */
-    public void buscaPalavra(String linha, int num) {
+    public void buscaPalavra(String linha, int num, String key2) {
         frase = linha.toCharArray();//passa a linha do arquivo para um vetor
+        List<Integer> posicoes = new ArrayList();//lista das posicoes iniciais do padrao
         /*
         cont -> contador de ocorrencias
         posicao -> posicao inicial do padrao
@@ -102,44 +92,55 @@ public class Algoritmo {
         j -> percorre a palavra chave
         i -> percorre a frase
         fim -> guarda o valor inicial de i
-        */
-        int cont = 0, posicao = -1, flag = 0, j = tam - 1, i = tam - 1, fim = tam-1;
+         */
+        int cont = 0, posicao = -1, flag = 0, j = tam - 1, i = tam - 1, fim = tam - 1;
+
         while (i < frase.length && j < tam) {
-            if (frase[i] == chave[j]) {//caso os caracteres sejam iguais 
-                while (frase[i] == chave[j]) {//enquanto os caracteres sao iguais
+            if (frase[i] == chave[j]) {
+                while (frase[i] == chave[j]) {
                     i--;
                     j--;
-                    if (j == 0) {//se chegou no inicio da palavra chave
+                    if (j < 0) {//se chegou no inicio da palavra chave
                         flag = 1;//achou o padrao
-                        posicao = i;//salva posicao inicial
+                        posicao = i + 1;//salva posicao inicial
+                        cont += 1;//incrementa o contador do padrao
                         break;
                     }
                 }
-            } else {//se os caracteres sao diferentes
-                i += valor[i];//salto
+            } else {
+                for (int a = 0; a < alfabeto.length; a++) {
+                    if (frase[i] == alfabeto[a]) {
+                        i += valor[a];//salto
+                        if (i >= frase.length) {
+                            break;
+                        }
+                    }
+                }
                 flag = 0;//nao achou o padrao
             }
-                j = tam - 1;//reposiciona o j no fim da palavra chave
-                fim = fim+1;//incrementa o fim
-                i = fim;//reposiciona o i na frase
+            j = tam - 1;//reposiciona o j no fim da palavra chave
+            fim = fim + 1;//incrementa o fim
+            i = fim;//reposiciona o i na frase
             if (flag == 1) {//se encontrou o padrao
-                cont += 1;
-                saidaArquivo(num, cont, posicao);//escreve no arquivo
+                posicoes.add(posicao);
             }
         }
+        saidaArquivo(cont, (ArrayList) posicoes, num);//escreve no arquivo saida1.txt
+        substituiPalavra(frase, key2, cont);//chamada da funcao de substituicao do padrao
     }
 
 
     /* funcao que cria arquivo de saida contendo a ocorrencia da palavra chave em cada linha do arquivo lido
     e a posicao inicial de cada ocorrencia da palavra */
-    public void saidaArquivo(int num, int cont, int posicao) {
+    public void saidaArquivo(int cont, ArrayList posicoes, int num) {
         try {
-            /* não aceita caracteres especiais */
             FileWriter saida = new FileWriter("saida1.txt", true);
             BufferedWriter sai = new BufferedWriter(saida);
 
             //escreve no arquivo
-            sai.write("Linha " + num + " - Número de ocorrências da palavra: " + cont + " Posição de ocorrência: " + posicao);
+            sai.write("Total de ocorrências na linha " + num + ": " + cont);
+            sai.newLine();
+            sai.write("Posições: " + posicoes);
             sai.newLine();
             sai.flush();
             sai.close();
@@ -149,4 +150,87 @@ public class Algoritmo {
         }
     }
 
+    public void substituiPalavra(char[] frase, String key2, int cont) {
+        chave2 = key2.toCharArray();//passa a segunda palavra chave para um vetor
+        int tamanho = chave2.length;//tamanho da segunda palavra chave
+        List<Character> fraseAux = new ArrayList();//lista que guarda os caracteres da frase
+        char[] frase2 = null;//vetor para a frase apos a substituicao
+
+        /* definicao do tamanho da frase2 */
+        if (tamanho > tam) {
+            frase2 = new char[frase.length + (cont * (tamanho - tam))];
+        } else {
+            frase2 = new char[frase.length];
+        }
+
+        /*
+            i -> contador da frase2
+            j -> contador da primeira palavra chave
+            k -> contador da frase
+            l -> contador auxiliar da frase
+            flag -> se for 0, achou o padrao. Se for 1, "quase" achou o padrao
+        */
+        int i = 0, j = 0, k = 0, l = 0, flag = 0;
+        while (k < frase.length) {
+            if (frase[k] == chave[j]) {
+                l = k;
+                while (frase[l] == chave[j]) {
+                    fraseAux.add(frase[l]);//salva a frase em um vetor auxiliar, caso nao ache o padrao
+                    flag = 1;
+                    j++;
+                    l++;
+                    if (j == chave.length) {//se achou o padrao, escreve a nova chave na frase2
+                        for (int n = 0; n < tamanho; n++) {
+                            frase2[i] = chave2[n];
+                            i++;
+                        }
+                        flag = 0;
+                        k += tam;
+                        break;
+                    }
+
+                    if (l >= frase.length) {//se leu toda a frase, escreve a lista auxiliar na frase2
+                        for (int m = 0; m < fraseAux.size(); m++) {
+                            frase2[i] = fraseAux.get(m);
+                            i++;
+                        }
+                        fraseAux.clear();//limpa a lista
+                        flag = 0;
+                        k += tam;
+                        break;
+                    }
+                }
+                if (flag == 1) {//se "quase" achou o padrao, escreve a lista auxiliar na frase2
+                    for (int m = 0; m < fraseAux.size(); m++) {
+                        frase2[i] = fraseAux.get(m);
+                        i++;
+                    }
+                    k += fraseAux.size();
+                }
+            } else {
+                frase2[i] = frase[k];
+                k++;
+                i++;
+            }
+            j = 0;
+            fraseAux.clear();//limpa a lista
+        }
+        saidaArquivo2(frase2);//escreve no arquivo saida2.txt
+    }
+
+    public void saidaArquivo2(char[] frase2) {
+        try {
+            FileWriter saida = new FileWriter("saida2.txt", true);
+            BufferedWriter sai = new BufferedWriter(saida);
+
+            //escreve no arquivo
+            sai.write(frase2);
+            sai.newLine();
+            sai.flush();
+            sai.close();
+
+        } catch (IOException ex) {
+            System.out.println("Erro ao escrever no arquivo saida1.txt");
+        }
+    }
 }
--- src/boyermoore/main.java
+++ src/boyermoore/main.java
@@ -1,3 +1,12 @@
+/*
+    TRABALHO DE ALGORITMOS E ESTRUTURA DE DADOS II
+    IMPLEMENTACAO DO ALGORITMO BOYER MOORE
+
+    PALOMA GOMES - 13.2.8564
+    TALITA SANTOS -  14.1.8335
+
+*/
+
 
 package boyermoore;
 
@@ -8,13 +17,17 @@ public class main {
     public static void main(String[] args) {
         
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite a palavra chave:");
+        System.out.println("Digite a palavra chave da primeira entrada:");
         String key = "";
         key = leitura.nextLine();
         
+        System.out.println("Digite a palavra chave da segunda entrada para substituição:");
+        String key2 = "";
+        key2 = leitura.nextLine();
+        
         Algoritmo a = new Algoritmo();
         a.preProcessamento(key);
-        a.leArquivo();
+        a.leArquivo(key2);
         
     }
 }
